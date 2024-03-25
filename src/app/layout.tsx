76862406a65d744b28eee1370d0bc491d7982c41
Charis Mytilinaios <charis.mytilinaios@gmail.com>
2024-03-25 12:00:02 +0200

    Initializes base styles and chadcnUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 import { FC, PropsWithChildren, ReactElement } from 'react';
+import '@/styles/main.scss';
 
 const inter = Inter({ subsets: [ 'latin' ] });
 
